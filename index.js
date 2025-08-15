bb9d9f42b4f312f93d192999646b3bc2aa1650d1
mnosumon <mdnuruddin378350@gmail.com>
2025-08-15 15:58:26 +0600

    user Route add

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("hello hame page");
+});
+app.get("/user", (req, res) => {
+  res.send("hello Users page");
 });
 
 app.listen(PORT, () => {
